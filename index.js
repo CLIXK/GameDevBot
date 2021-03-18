c961a92f9b6d4aa2e9943e14fc0e399891b855cd
Jaidon Nelson <jaidonkdnelson@gmail.com>
2021-03-17 21:23:54 -0400

    Add eslint and prettier

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-const Discord = require('discord.js') 
+const Discord = require('discord.js');
 const bot = new Discord.Client();
-const fs = require("fs")
+const fs = require('fs');
 bot.commands = new Discord.Collection();
 
 bot.on('ready', () => {
-    console.log('Bot online')
+    console.log('Bot online');
 
     fs.readdir('./commands', (err, files) => {
-        if(err) return console.log(err);
+        if (err) return console.log(err);
 
-        let jsfile = files.filter(f => f.split(".").pop() == 'js');
+        const jsfile = files.filter((f) => f.split('.').pop() == 'js');
 
+        if (jsfile.length <= 0) return console.log('Could not find commands!');
 
-        if (jsfile.length <= 0) return console.log("Could not find commands!")
-
-        jsfile.forEach(f => {
-            let props = require(`./commands/${f}`);
-            bot.commands.set(props.help.name, props)
-        })
-    })
-})
+        jsfile.forEach((f) => {
+            const props = require(`./commands/${f}`);
+            bot.commands.set(props.help.name, props);
+        });
+    });
+});
 
 bot.on('message', (message) => {
-    if(message.author.bot) return;
-    if(message.channel.type !== 'text') return;
-    let prefix = '!';
+    if (message.author.bot) return;
+    if (message.channel.type !== 'text') return;
+    const prefix = '!';
     // hello there ['hello', 'there']
     // !ban user reason ['user', 'reason']
     // Breaking Rules ['breaking', 'rules'] breaking rules
     // hello
-    let MessageArray = message.content.split(' ');
-    let cmd = MessageArray[0].slice(prefix.length)
-    let args = MessageArray.slice(1)
+    const MessageArray = message.content.split(' ');
+    const cmd = MessageArray[0].slice(prefix.length);
+    const args = MessageArray.slice(1);
 
-    if(!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(prefix)) return;
 
-    let commandfile = bot.commands.get(cmd);
-    if(commandfile) {commandfile.run(bot,message,args)}
+    const commandfile = bot.commands.get(cmd);
+    if (commandfile) {
+        commandfile.run(bot, message, args);
+    }
+});
 
-})
-
-bot.login("ODE4NTczNTI4ODcyOTEwODQ5.YEaCAQ.WB_kUQcSnP2IWeqEo1bJohJr7bo")
+bot.login('ODE4NTczNTI4ODcyOTEwODQ5.YEaCAQ.WB_kUQcSnP2IWeqEo1bJohJr7bo');
